e731a9017d429ef84a2ef87063a50f3498d852ff
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-04-29 17:21:17 -0300

    feat: adicionando o blur nas mensagens ditas como negativas

--- content.js
+++ content.js
@@ -8,23 +8,36 @@ function catching_comments() {
 function msg_comprimid () {
     coments = catching_comments()
     coments.forEach(function(user_coments){
-        fetchData(user_coments.innerText)
+        fetchData(user_coments)
     });
     
     }
 
+function change_blur(element)  {
+    element.style.filter = "blur(0px)"
+}
+
 async function fetchData (text)  {
     const url = "https://sentimentai-api.onrender.com/predict"
     
+
+
     try {
-        const body = {"text": text}
+        const body = {"text": text.innerText}
         const response = await fetch(url, {
             method: "POST",
             headers: {"content-type": "application/json"},
             body: JSON.stringify(body)
         })
         const data = await response.json()
+        
         console.log(data)
+
+        if (data.sentiment == "negative") {
+            console.log('negative')
+            text.style.filter = "blur(5px)"
+            text.addEventListener("click", () => change_blur(text))
+    }
     }
 
     catch (error) {
