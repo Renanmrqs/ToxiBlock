41efa3ebb7d3069e15187d4b33e5c0485f8d8e99
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-04-29 14:49:37 -0300

    feat: adicionando a funcao que captura os comentarios e envia para a API

--- content.js
+++ content.js
@@ -8,10 +8,30 @@ function catching_comments() {
 function msg_comprimid () {
     coments = catching_comments()
     coments.forEach(function(user_coments){
-        console.log(user_coments.innerText)
+        fetchData(user_coments.innerText)
     });
     
     }
+
+async function fetchData (text)  {
+    const url = "https://sentimentai-api.onrender.com/predict"
+    
+    try {
+        const body = {"text": text}
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(body)
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+    
+}
 
 msg_comprimid()
 
