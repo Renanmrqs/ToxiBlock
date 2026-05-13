5c003148a0dda0a7d8b1a2a41f07dac6f0469b11
Renanmrqs <renanmarques1923@gmail.com>
2026-05-13 16:19:36 -0300

    feature: ajustes na estilizacao do botao

--- popup.js
+++ popup.js
@@ -12,14 +12,33 @@ function send_off(element)  {
     )
 }
 
+function send_off_css(btn_off, btn_on) {
+  btn_off.classList.remove('inactive');
+  btn_on.classList.add('inactive');
+}
+
+function send_on_css(btn_on, btn_off) {
+  btn_on.classList.remove('inactive');
+  btn_off.classList.add('inactive');
+}
+
 function send_btn_info() {
     const btn_on = document.querySelector("#power_on")
     const btn_off = document.querySelector("#power_off")
     
-    btn_off.addEventListener("click", () => send_off(btn_off))
+    btn_off.addEventListener("click", () => {
+        send_off(btn_off); 
+        send_off_css(btn_off, btn_on)
+    })
     
-    btn_on.addEventListener("click", () => send_on(btn_on))
+    btn_on.addEventListener("click", () => {
+        send_on(btn_on); 
+        send_on_css(btn_on, btn_off)
+    })
     
 }
 
 send_btn_info()
+
+
+
